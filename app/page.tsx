4a546469f8de971f8a04bf43e4301020bf497bbe
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-11-23 22:34:23 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <main className="relative w-full dark:bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="w-full max-w-7xl ">
+      <div className="w-full max-w-8xl">
         <Hero />
       </div>
     </main>
