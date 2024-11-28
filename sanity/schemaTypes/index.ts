0aaca667eddf5623cbd4ddbfb6a9d4d86df139e9
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-11-28 20:25:34 +0500

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import post from './post'
 import signatureProjects from './signatureProjects'
+import education from './education'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, signatureProjects],
+  types: [post, signatureProjects, education],
 }
