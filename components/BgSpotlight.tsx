7aa94d82fb00686b3018d647466edde6071bffc8
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-12-01 15:28:03 +0500

    chore: working on UI/UX experience

--- components/BgSpotlight.tsx
+++ components/BgSpotlight.tsx
@@ -1,25 +1,59 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { Spotlight } from "./ui/Spotlight";
 
-
-
 const BgSpotlight = () => {
+  const [positions, setPositions] = useState([
+    { x: 0, y: 0 }, // First spotlight
+    { x: 0, y: 0 }, // Second spotlight
+    { x: 0, y: 0 }, // Third spotlight
+  ]);
 
- 
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const { clientX: mouseX, clientY: mouseY } = e;
+
+      setPositions([
+        { x: mouseX / 20, y: mouseY / 20 },
+        { x: mouseX / 15, y: mouseY / 15 },
+        { x: mouseX / 10, y: mouseY / 10 },
+      ]);
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("Positions updated:", positions);
+  }, [positions]);
 
   return (
-    <div>
+    <div className="relative">
       <Spotlight
-        className="lg:top-28 lg:-left-24 h-[50vh] w-[60vw] sm:top-10 sm:-left-20 md:top-20 md:-left-20 top-14 -left-10"
-        fill="gray"
+        className=" lg:top-28 lg:-left-24 h-[50vh] w-[30vw] sm:top-10 sm:-left-20 md:top-20 md:-left-20 top-14 -left-10 transition-transform duration-300 ease-out"
+        fill="red"
+        style={{
+          transform: `translate(${positions[0].x}px, ${positions[0].y}px)`,
+        }}
       />
       <Spotlight
-        className="lg:-top-40 lg:left-[300px] h-[80vh] w-[80vw] sm:-top-28 sm:left-28 md:-top-40 md:left-20 -top-28 left-14"
-        fill="blue"
+        className=" lg:-top-40 lg:left-[300px] h-[80vh] w-[80vw] sm:-top-28 sm:left-28 md:-top-40 md:left-20 -top-28 left-14 transition-transform duration-300 ease-out"
+        fill="black"
+        style={{
+          transform: `translate(${positions[1].x}px, ${positions[1].y}px)`,
+        }}
       />
       <Spotlight
-        className="lg:top-0 lg:left-20 h-[60vh] w-[50vw] sm:top-4 sm:left-20 md:top-20 md:left-40  top-10 left-20"
-        fill={`skyblue`}
+        className=" lg:top-0 lg:left-20 h-[60vh] w-[50vw] sm:top-4 sm:left-20 md:top-20 md:left-40 top-10 left-20 transition-transform duration-300 ease-out"
+        fill="purple"
+        style={{
+          transform: `translate(${positions[2].x}px, ${positions[2].y}px)`,
+        }}
       />
     </div>
   );
--- components/ui/Spotlight.tsx
+++ components/ui/Spotlight.tsx
@@ -4,18 +4,20 @@ import { cn } from "@/utils/cn";
 type SpotlightProps = {
   className?: string;
   fill?: string;
+  style?: React.CSSProperties;
 };
 
-export const Spotlight = ({ className, fill }: SpotlightProps) => {
+export const Spotlight = ({ className, fill, style }: SpotlightProps) => {
   return (
     <svg
       className={cn(
-        "animate-spotlight pointer-events-none absolute z-[1]  h-[169%] w-[138%] lg:w-[84%] opacity-0",
+        "pointer-events-none absolute z-[1] h-[169%] w-[138%] lg:w-[84%] opacity-100 transition-transform",
         className
       )}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 3787 2842"
       fill="none"
+      style={style}
     >
       <g filter="url(#filter)">
         <ellipse
